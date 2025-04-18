d2b977801d0637448f7ace0069fa3785172c8108
Elena Gogina <lenochkamappiece@yandex.ru>
2025-04-18 10:27:25 +0300

    add field age

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Cat, Owner, Achievement, AchievementCat
+import datetime as dt
 
 
 class AchievementSerializer(serializers.ModelSerializer):
@@ -12,10 +13,15 @@ class AchievementSerializer(serializers.ModelSerializer):
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
