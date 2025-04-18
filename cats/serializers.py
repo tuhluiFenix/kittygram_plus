31e6f0394ae746a9b7eb0f3bdb60fd77a7e46380
Elena Gogina <lenochkamappiece@yandex.ru>
2025-04-18 09:26:26 +0300

    add fields owner & cats

--- cats/serializers.py
+++ cats/serializers.py
@@ -4,14 +4,16 @@ from .models import Cat, Owner
 
 
 class CatSerializer(serializers.ModelSerializer):
+    owner = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Owner
-        fields = ('first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'cats')
